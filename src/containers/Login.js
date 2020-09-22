810fe276774474f303d4f851397965d653bd3109
pabloVoorvaart <pablov@graficadirecta.com>
2020-09-22 18:33:42 +0200

    disabled register

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button, Row, Col } from "react-bootstrap";
+import { Form, Button, Row, Col, InputGroup } from "react-bootstrap";
 import { Auth } from "aws-amplify";
 import "./Login.css";
 import Logo from "../img/logo.svg";
@@ -7,29 +7,19 @@ import { useAppContext } from "../libs/contextLib";
 import { useHistory } from "react-router-dom";
 import Footer from "../components/Footer.js";
 
-
 export default function Login(props) {
   const [password, setPassword] = useState("");
   const [confirmationCode, setConfirmationCode] = useState("");
   const [email, setEmail] = useState("");
   const [isRegister, setIsRegister] = useState(false);
   const [isConfirmation, setIsConfirmation] = useState(false);
+  const [validated, setValidated] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const { userHasAuthenticated } = useAppContext();
   const history = useHistory();
 
-
-  const validate = () => {
-    if (email !== "" && password !== "") {
-      return true;
-    }
-    return false;
-  };
-
-  const validateCode = () => {
-    return true;
-  };
-
+  /*
   async function handleLogin(event) {
     event.preventDefault();
     if (validate()) {
@@ -41,51 +31,71 @@ export default function Login(props) {
         if (e.code === "UserNotConfirmedException") {
           setIsRegister(true);
           setIsConfirmation(true);
-        } else {
-          alert("La contraseña y el usuario no coinciden.");
-        }
+        } 
       }
-    } else {
-      alert("Porvafor rellene todos los campos.");
-    }
+    } 
   }
+  */
 
   async function handleRegistration(event) {
     event.preventDefault();
-    if (validate()) {
+    const form = event.currentTarget;
+    console.log(form);
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }else{
       try {
         await Auth.signUp(email, password);
-        alert("Registration successful!");
         setIsConfirmation(true);
+        return;
       } catch (e) {
-        console.log(e.message);
+        setErrorMessage("error");
       }
-    } else {
-      alert("Porvafor rellene todos los campos.");
     }
+    setValidated(true);
   }
 
   async function handleConfirmation(event) {
     event.preventDefault();
-    if (validateCode()) {
-      try {
-        await Auth.confirmSignUp(email, confirmationCode);
-        alert("Confirmation successful!");
-        setIsRegister(false);
-        setIsConfirmation(false);
-      } catch (e) {
-        console.log(e.message);
-      }
-    } else {
-      alert("Porvafor rellene todos los campos.");
+    try {
+      await Auth.confirmSignUp(email, confirmationCode);
+      setIsRegister(false);
+      setIsConfirmation(false);
+    } catch (e) {
+      console.log(e.message);
     }
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    console.log(form);
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    try {
+      await Auth.signIn(email, password);
+      userHasAuthenticated(true);
+      history.push("/chainlog");
+    } catch (e) {
+      if (e.code === "UserNotConfirmedException") {
+        setIsRegister(true);
+        setIsConfirmation(true);
+      }
+    }
+
+    setValidated(true);
   }
 
   const renderLogin = () => {
     return (
-      <Form>
-        <Form.Group controlId="formBasicPassword">
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form.Group controlId="formBasicEmail">
           <Form.Control
+            required
             value={email}
             onChange={(e) => {
               setEmail(e.target.value);
@@ -93,6 +103,9 @@ export default function Login(props) {
             type="email"
             placeholder="Email"
           />
+          <Form.Control.Feedback type="invalid">
+            Por favor rellene este campo con un email valido.
+          </Form.Control.Feedback>
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
           <Form.Control
@@ -101,58 +114,99 @@ export default function Login(props) {
               setPassword(e.target.value);
             }}
             type="password"
-            placeholder="Password"
+            placeholder="Contraseña"
+            required
           />
+          <Form.Control.Feedback type="invalid">
+            Por favor rellene este campo.
+          </Form.Control.Feedback>
         </Form.Group>
         <div className="buttonLayout">
-          <Button className="loginButton" onClick={handleLogin} type="submit">
+          <Button className="loginButton" type="submit">
             Entrar
           </Button>
           <Button
             onClick={(e) => {
+              e.preventDefault();
               setIsRegister(true);
+              setValidated(false);
             }}
-            type="submit"
           >
-            Registrate
+            Regístrate
           </Button>
         </div>
       </Form>
     );
   };
 
+
+  const renderRegister = () => {
+    return (
+      <div>
+        <p className="text">Esta funcionalidad esta desactivada</p>
+        <div className="buttonLayout">
+            <Button
+              className="backwardsButton"
+              onClick={(e) => {
+                e.preventDefault();
+                setIsRegister(false);
+                setValidated(false);
+              }}
+            >
+              &lt;
+            </Button>
+          </div>
+      </div>
+    )
+  }
+/*
   const renderRegister = () => {
     if (!isConfirmation) {
       return (
-        <Form>
-          <Form.Group controlId="formBasicPassword">
-            <Form.Control
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              type="email"
-              placeholder="Email"
-            />
-          </Form.Group>
-          <Form.Group controlId="formBasicPassword">
+        <Form noValidate validated={validated} onSubmit={handleRegistration}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Control
+            required
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="email"
+            placeholder="Email"
+          />
+          <Form.Control.Feedback type="invalid">
+            Por favor rellene este campo con un email valido.
+          </Form.Control.Feedback>
+        </Form.Group>
+          <Form.Group controlId="formRegisterPassword">
             <Form.Control
               value={password}
               onChange={(e) => {
                 setPassword(e.target.value);
               }}
               type="password"
-              placeholder="Password"
+              placeholder="Contraseña"
             />
+            <Form.Control.Feedback type="invalid">
+              Por favor rellene este campo.
+            </Form.Control.Feedback>
           </Form.Group>
-		  <div className="buttonLayout">
-		  	<Button className="backwardsButton" onClick={(e) => {e.preventDefault(); setIsRegister(false);}}>
+          <div className="errorMessage">
+            {errorMessage}
+          </div>
+          <div className="buttonLayout">
+            <Button
+              className="backwardsButton"
+              onClick={(e) => {
+                e.preventDefault();
+                setIsRegister(false);
+                setValidated(false);
+              }}
+            >
               &lt;
             </Button>
-            <Button onClick={handleRegistration}>
-              Register
-            </Button>
-		  </div>	
+            <Button type="submit">Registrarse</Button>
+          </div>
         </Form>
       );
     } else {
@@ -168,30 +222,40 @@ export default function Login(props) {
               placeholder="Codigo de confirmación"
             />
           </Form.Group>
-		  <div className="buttonLayout">
-		  	<Button className="backwardsButton" onClick={(e) => {e.preventDefault(); setIsRegister(false);}}>
+          <div className="buttonLayout">
+            <Button
+              className="backwardsButton"
+              onClick={(e) => {
+                e.preventDefault();
+                setIsRegister(false);
+              }}
+            >
               &lt;
             </Button>
-          <Button onClick={handleConfirmation} variant="primary" type="submit">
-            Entrar
-          </Button>
-		  </div>
+            <Button
+              onClick={handleConfirmation}
+              variant="primary"
+              type="submit"
+            >
+              Entrar
+            </Button>
+          </div>
         </Form>
       );
     }
   };
-
+*/
   return (
     <>
-    <div className="loginForm">
-      <Row>
-        <Col md={4}>
-          <img className="loginLogo" src={Logo} alt="React Logo" />
-        </Col>
-        <Col md={8}>{isRegister ? renderRegister() : renderLogin()}</Col>
-      </Row>
-    </div>
-    <Footer />
+      <div className="loginForm">
+        <Row>
+          <Col md={4}>
+            <img className="loginLogo" src={Logo} alt="React Logo" />
+          </Col>
+          <Col md={8}>{isRegister ? renderRegister() : renderLogin()}</Col>
+        </Row>
+      </div>
+      <Footer />
     </>
   );
 }
